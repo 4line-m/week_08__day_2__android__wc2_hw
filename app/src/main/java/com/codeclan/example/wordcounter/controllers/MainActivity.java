df5e817e1bc858064f78f0588e2698c2c5d4b6bf
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-11-14 21:54:39 +0000

    added About and Settings activities

--- app/src/main/java/com/codeclan/example/wordcounter/controllers/MainActivity.java
+++ app/src/main/java/com/codeclan/example/wordcounter/controllers/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -66,4 +69,22 @@ public class MainActivity extends AppCompatActivity {
         textView.setText(wordCounter.toString());
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_about){
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+        } else if (item.getItemId() == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        }
+        return true;
+    }
 }
